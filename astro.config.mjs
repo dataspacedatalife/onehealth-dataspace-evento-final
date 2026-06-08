6e93457110583551a560ccfd5230a6eb38816912
Diana <dianaramostorrado@gmail.com>
2026-06-08 13:38:20 +0200

    deploy files cleaned

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+ // @ts-check
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dataspacedatalife.github.io',
-  base: '/onehealth-dataspace-evento-final',
+  base: '/onehealth-dataspace-evento-final/',
+
   integrations: [react()],
 
   vite: {
